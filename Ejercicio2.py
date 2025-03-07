b96cece006f1442000b898a863c268d536e7ff27
Joan Sebastian Cardozo Salinas <84690368+PintoDev64@users.noreply.github.com>
2025-03-07 01:59:00 +0000

    Parcial terminado

---
+++ Ejercicio2.py
@@ -0,0 +1,6 @@
+Base = int(input("Ingrese la base del rectángulo: "))
+Altura = int(input("Ingrese la altura del rectángulo: "))
+
+AreaRectangulo = Base * Altura
+
+print(f"El area del rectangulo es: {AreaRectangulo}")
